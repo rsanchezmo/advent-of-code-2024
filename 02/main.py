a336af34968cede49d34750b25a912bcfebd31d5
Sánchez Molina, Rodrigo <rsanchezmo@indra.es>
2024-12-02 11:41:36 +0100

    day 02 solution

---
+++ 02/main.py
@@ -0,0 +1,61 @@
+import time
+
+def parse_input(file: str):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    reports = [list(map(int, line.split(' '))) for line in lines]
+    return reports
+
+def is_safe_report(report):
+    diff = report[0] - report[1]
+    if abs(diff) > 3 or diff == 0:
+        return False
+
+    increasing = diff < 0
+
+    for i in range(1, len(report)-1):
+        diff = report[i] - report[i+1]
+        if abs(diff) > 3 or diff == 0 or (increasing and diff > 0) or (not increasing and diff < 0):
+            return False
+
+    return True
+
+def part_a(file: str)  -> int:
+    reports = parse_input(file)
+
+    safe_reports = 0
+    for report in reports:
+        if is_safe_report(report):
+            safe_reports += 1
+
+    return safe_reports
+
+def part_b(file: str) -> int:
+    reports = parse_input(file)
+    safe_reports = 0
+    for report in reports:
+        for level_idx in range(len(report)):
+            new_report = report[:level_idx] + report[level_idx+1:]
+            if is_safe_report(new_report):
+                safe_reports += 1
+                break
+
+    return safe_reports
+
+if __name__ == '__main__':
+
+    file = './02/input.txt'
+
+    init_t = time.perf_counter()
+    part_a_sol = part_a(file)
+    end_t = time.perf_counter()
+    elapsed = end_t - init_t
+    print(f'Part A: {part_a_sol}')
+    print(f'Part A: {elapsed * 1e3:.2f} ms')
+
+    init_t = time.perf_counter()
+    part_b_sol = part_b(file)
+    end_t = time.perf_counter()
+    elapsed = end_t - init_t
+    print(f'Part B: {part_b_sol}')
+    print(f'Part B: {elapsed * 1e3:.2f} ms')
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Advent of Code 2024
-My **python** solutions to [AoC 2024](https://adventofcode.com/2024)
+My **python** solutions to [AoC 2024](https://adventofcode.com/2024). Second year in a row!
 
 # Solutions
 - Day 1
+- Day 2
